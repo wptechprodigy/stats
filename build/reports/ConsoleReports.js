451db67f58ad84a41b66c45f3f0d9add358dcb42
wptechprodigy <waheed.freelancer16@gmail.com>
2020-04-03 15:12:14 +0100

    add console report delegate

---
+++ build/reports/ConsoleReports.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var ConsoleReport = /** @class */ (function () {
+    function ConsoleReport(report) {
+        this.report = report;
+    }
+    ConsoleReport.prototype.print = function (report) {
+    };
+    return ConsoleReport;
+}());
+exports.ConsoleReport = ConsoleReport;
